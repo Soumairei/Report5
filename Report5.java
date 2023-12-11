916808661781f78eeb70bc1dc31136da5f1e7c48
Soumairei <e235739@ie.u-ryukyu.ac.jp>
2023-12-11 20:11:59 +0900

    step2

--- Report5.java
+++ Report5.java
@@ -1,14 +1,19 @@
 public class Report5{
     public static void main(String[]args){
         String str=null;
-        
+        try{
             
         
         System.out.println(str.length());
 
-        
+        }catch(java.lang.NumberFormatException e){
+            System.out.println("java.lang.NumberFormatExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }
        
 
     }
 
 }
+
+

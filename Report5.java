e89a2ce554b68807dda36e8d2554079ab60b91e7
Soumairei <e235739@ie.u-ryukyu.ac.jp>
2023-12-11 19:11:13 +0900

    report5-1

---
+++ Report5.java
@@ -0,0 +1,9 @@
+public class Report5{
+    public static void main(String[]args){
+        String str=null;
+        int value = Integer.parseInt(str);
+        System.out.println(value);
+
+    }
+
+}
